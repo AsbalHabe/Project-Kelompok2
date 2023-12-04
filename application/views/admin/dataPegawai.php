974a33c7d0bf48cdbf9c247b6ddea06d27c44343
wendnsyh <wendy.martadiandsyah75@gmail.com>
2023-12-04 23:55:04 +0700

    Membuat Table untuk data pegawai

--- application/views/admin/dataPegawai.php
+++ application/views/admin/dataPegawai.php
@@ -10,4 +10,37 @@
 
     <a class="mb-2 mt-2 btn btn-sm btn-success" href="<?php echo base_url('admin/dataPegawai/tambahData') ?>"><i class="fas fa-plus">Tambah Pegawai</i></a>
 
+    <table class="table table-striped table-bordered">
+        <tr>
+            <th class="text-center">No</th>
+            <th class="text-center">Nik</th>
+            <th class="text-center">Nama Pegawai</th>
+            <th class="text-center">Jenis Kelamin</th>
+            <th class="text-center">Jabatan</th>
+            <th class="text-center">Tanggal Masuk</th>
+            <th class="text-center">Status</th>
+            <th class="text-center">Photo</th>
+            <th class="text-center">Action</th>
+        </tr>
+
+        <?php $no=1; foreach($pegawai as $p) : ?>
+            <tr>
+                <td><?php echo $no++ ?></td>
+                <td><?php echo $p->nik ?></td>
+                <td><?php echo $p->nama_pegawai ?></td>
+                <td><?php echo $p->jenis_kelamin ?></td>
+                <td><?php echo $p->jabatan ?></td>
+                <td><?php echo $p->tanggal_masuk ?></td>
+                <td><?php echo $p->status ?></td>
+                <td><img src="<?php echo base_url().'assets/Foto/'.$p->foto ?>" width="75px"></td>
+                <td>
+                <div class="text-center">
+                        <a class="btn btn-sm btn-primary" href="<?php echo base_url('admin/dataPegawai/updateData/' . $p->id_pegawai); ?>"><i class="fas fa-edit"></i></a>
+                        <a onclick="return confirm('Yakin Hapus?')" class="btn btn-sm btn-danger" href="<?php echo base_url('admin/dataPegawai/deleteData/' . $p->id_pegawai); ?>"><i class="fas fa-trash"></i></a>
+                    </div>
+                </td>
+                
+            </tr>
+        <?php endforeach; ?>
+    </table>
 </div>
