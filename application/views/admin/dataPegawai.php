d534749235c53fa6a59903cb8232ca2699469baf
wendnsyh <wendy.martadiandsyah75@gmail.com>
2023-12-04 23:33:52 +0700

    Membuat Flash data pada file views>dataPegawai

--- application/views/admin/dataPegawai.php
+++ application/views/admin/dataPegawai.php
@@ -6,5 +6,8 @@
         <h4 class="h3 mb-0 text-gray-800"><?php echo $title ?></h4>
     </div>
 
+    <?php echo $this->session->flashdata('pesan') ?>
+
+    <a class="mb-2 mt-2 btn btn-sm btn-success" href="<?php echo base_url('admin/dataPegawai/tambahData') ?>"><i class="fas fa-plus">Tambah Pegawai</i></a>
 
 </div>
