dd0ba53e5cbc468db981f064832e6fe97cdcbb10
wendnsyh <wendy.martadiandsyah75@gmail.com>
2023-12-04 23:58:59 +0700

    Membuat Function baru yaitu tambah data pada controller data pegawai

--- application/controllers/Admin/dataPegawai.php
+++ application/controllers/Admin/dataPegawai.php
@@ -15,4 +15,19 @@ class dataPegawai extends CI_Controller{
         $this->load->view('admin/dataPegawai', $data);
         $this->load->view('template_admin/footer_admin');
     }
+
+    public function tambahData()
+    {
+       // Set judul halaman
+       $data['title'] = "Tambah Data Pegawai";
+        
+       // Mengambil data jabatan dari model
+       $data['pegawai'] = $this->PenggajianModel->get_data('data_pegawai')->result();
+       
+       // Memuat tampilan dengan template
+       $this->load->view('template_admin/header_admin', $data);
+       $this->load->view('template_admin/sidebar_admin');
+       $this->load->view('admin/dataPegawai', $data);
+       $this->load->view('template_admin/footer_admin');
+    }
 }
