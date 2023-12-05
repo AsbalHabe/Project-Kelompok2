e7292d31b4e3bcdd83e965bb8282bc212225bfc6
wendnsyh <wendy.martadiandsyah75@gmail.com>
2023-12-05 22:50:32 +0700

    Memberi waran pada ikon, data jabatan, pegawai, admin, kehadiran pada menu dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -12,13 +12,13 @@
 
         <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card border-left-primary shadow h-100 py-2 bg-dark">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                            <div class="text-xs font-weight-bold text-light text-uppercase mb-1">
                                 Data Pegawai</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $pegawai ?></div>
+                            <div class="h5 mb-0 font-weight-bold text-light"><?php echo $pegawai ?></div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-users fa-2x text-gray-300"></i>
@@ -30,13 +30,13 @@
 
         <!-- Earnings (Monthly) Card Example (Data Admin) -->
         <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
+            <div class="card border-left-success shadow h-100 py-2 bg-danger">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                            <div class="text-xs font-weight-bold text-light text-uppercase mb-1">
                                 Data Admin</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $admin ?></div>
+                            <div class="h5 mb-0 font-weight-bold text-light"><?php echo $admin ?></div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-user-cog fa-2x text-gray-300"></i>
@@ -47,15 +47,15 @@
         </div>
         <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-info shadow h-100 py-2">
+            <div class="card border-left-info shadow h-100 py-2 bg-primary">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Data Jabatan
+                            <div class="text-xs font-weight-bold text-light text-uppercase mb-1">Data Jabatan
                             </div>
                             <div class="row no-gutters align-items-center">
                                 <div class="col-auto">
-                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $jabatan ?></div>
+                                    <div class="h5 mb-0 mr-3 font-weight-bold text-light"><?php echo $jabatan ?></div>
                                 </div>
                             </div>
                         </div>
@@ -69,13 +69,13 @@
 
         <!------Data Kehadiran------>
         <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-warning shadow h-100 py-2">
+            <div class="card border-left-warning shadow h-100 py-2 bg-success">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                            <div class="text-xs font-weight-bold text-light text-uppercase mb-1">
                                 Data Kehadiran</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $kehadiran ?></div>
+                            <div class="h5 mb-0 font-weight-bold text-light"><?php echo $kehadiran ?></div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-user fa-2x text-gray-300"></i>
@@ -84,10 +84,9 @@
                 </div>
             </div>
         </div>
+
+
     </div>
-
-
-</div>
 
 </div>
 <!-- End of Page Content -->
