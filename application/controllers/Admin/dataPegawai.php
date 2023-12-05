17b4dfcb73e10fc7484563c78178273f555a7b28
wendnsyh <wendy.martadiandsyah75@gmail.com>
2023-12-05 23:23:16 +0700

    membuat function baru di controller dataJabatan yaitu tambahDataAksi

--- application/controllers/Admin/dataPegawai.php
+++ application/controllers/Admin/dataPegawai.php
@@ -30,4 +30,49 @@ class dataPegawai extends CI_Controller{
        $this->load->view('admin/formTambahPegawai', $data);
        $this->load->view('template_admin/footer_admin');
     }
+
+    public function tambahDataAksi()
+    {
+        $this->_rules();
+
+        if($this->form_validation->run() == FALSE){
+        }else{
+            $nik                        = $this->input->post('nik');
+            $nama_pegawai               = $this->input->post('nama_pegawai');
+            $jenis_kelamin              = $this->input->post('jenis_kelamin');
+            $tanggal_masuk              = $this->input->post('tanggal_masuk');
+            $jabatan                    = $this->input->post('jabatan');
+            $status                     = $this->input->post('status');
+            $foto                      = $_FILES['foto'] ['name'];
+            if($foto=''){}else{
+                $config ['upload_path']   ='.assets/foto'
+                $config['allowed_types']  ='jpg|png|jpeg|gif';
+                $this->load->library('upload',$config);
+                if(!$this->upload->do_upload('photo')){
+                    echo "Foto Gagal di Upload!";
+                }else{
+                    $foto = $this->upload->data('file_name');
+                }
+            }
+
+            $data = array(
+                'nik'                       => $nik,
+                'nama_pegawai'              => $nama_pegawai,
+                'jenis_kelamin'             => $jenis_kelamin,
+                'jabatan'                   => $jabatan,
+                'tanggal_masuk'             => $tanggal_masuk,
+                'status'                    => $status,
+                'foto'                      => $foto,
+            );
+            
+            $this->PenggajianModel->insert_data($data, 'data_pegawai');
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>Data Berhasil ditambahkan !</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+                </div>');
+            redirect('admin/dataPegawai');
+        }
+    }
 }
