581b9db192b3ba78673c49e09d24f92c407f2fa3
wendnsyh <wendy.martadiandsyah75@gmail.com>
2023-12-09 16:52:46 +0700

    menambahkan beberapa fitur pada views data abasensi

--- application/views/admin/dataAbsensi.php
+++ application/views/admin/dataAbsensi.php
@@ -4,7 +4,66 @@
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800"><?php echo $title ?></h1>
-        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+    </div>
+    <div class="card mb-3">
+        <div class="card-header bg-primary text-white">
+            Filter Data Absensi
+        </div>
+        <div class="card-body">
+
+            <form class="form-inline">
+                <div class="form-group mb-2">
+                    <label for="staticEmail12">Bulan</label>
+                    <select class="form-control ml-2" name="bulan">
+                        <option value="">---Pilih Tahun-----</option>
+                        <option value="01">Januari</option>
+                        <option value="02">Februari</option>
+                        <option value="03">Maret</option>
+                        <option value="04">April</option>
+                        <option value="05">Mei</option>
+                        <option value="06">Juni</option>
+                        <option value="07">Juli</option>
+                        <option value="08">Agustus</option>
+                        <option value="09">September</option>
+                        <option value="10">Oktober</option>
+                        <option value="11">November</option>
+                        <option value="12">Desember</option>
+                    </select>
+                </div>
+
+                <div class="form-group mb-2 ml-5">
+                    <label for="staticEmail12">Tahun </label>
+                    <select class="form-control ml-2" name="tahun">
+                        <option value="">----Pilih Tahun---</option>
+                        <?php $tahun = date('Y');
+                        for ($i = 2019; $i < $tahun + 10; $i++) { ?>
+                            <option value="<?php echo $i ?>"><?php echo $i ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+
+                <button type="submit" class="btn btn-primary mb-2 ml-auto"><i class="far fa-eye-slash"></i> Tampilkan Data</button>
+                <a href="" class="btn btn-success mb-2 ml-3"><i class="fas fa-plus">Input Kehadiran</i></a>
+            </form>
+
+        </div>
+    </div>
+
+    <?php
+    if ((isset($_GET['bulan']) && $_GET['bulan']!='') && (isset($_GET['tahun']) &&
+     $_GET['tahun']!='')){
+        $bulan = $_GET['bulan'];
+        $tahun = $_GET['tahun'];
+        $bulantahun = $bulan.$tahun;
+     }else{
+        $bulan = date('m');
+        $tahun = date('Y');
+        $bulantahun = $bulan.$tahun;
+     }
+    ?>
+    <div class="alert alert-info">
+        Menampilkan Data Kehadiran Pegawai Bulan: <span class="font-weight-bold"><?php echo $bulan ?> </span> 
+        Tahun:<span class="font-weight-bold"><?php echo $tahun ?> </span>
     </div>
 
 
