116d1b4a3598354eff0645ba7f38a2ac90bbf153
AsbalHabe <ryanalfareth@gmail.com>
2023-12-15 17:01:11 +0700

    mengedit sedikit

--- application/views/template_admin/sidebar_admin.php
+++ application/views/template_admin/sidebar_admin.php
@@ -7,7 +7,7 @@
         <ul class="navbar-nav bg-success sidebar sidebar-light accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard">
                 <div class="sidebar-brand-icon brg-li">
                     <i class="fas fa-hospital text-white"></i>
                 </div>
